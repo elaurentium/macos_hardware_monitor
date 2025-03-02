7c62cea35f20f8cc9bf0334c8ecb434da85da7ea
elaurentium <credopuke@icloud.com>
2025-03-02 18:07:54 -0300

    remove buid

--- menu/.build/arm64-apple-macosx/debug/RustBridge.build/RustBridge.m.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.1.sdk/usr/include/Darwin.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.1.sdk/usr/include/mach-o/module.modulemap \
-  /Users/elaurentium/Documents/macos_hardware_monitor/menu/menuBar/RustBridge.m \
-  /Users/elaurentium/Documents/macos_hardware_monitor/menu/menuBar/include/RustBridge.h
